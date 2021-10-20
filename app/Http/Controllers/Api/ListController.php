8214f4a8c81227e71251d224e9ef881673db28de
Carlos Oliveira <cadu.oliveira1981@gmail.com>
2021-10-20 14:39:04 -0300

    backend

--- app/Http/Controllers/Api/ListController.php
+++ app/Http/Controllers/Api/ListController.php
@@ -3,41 +3,33 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
-use App\Helpers\GitHubConnection;
+use App\Services\ListService;
 
 class ListController extends Controller {
 
-    private $github;
+    private $service;
 
-    public function __construct(GitHubConnection $github) {
-        $this->github = $github;
+    public function __construct(ListService $service) {
+        $this->service = $service;
     }
     
     public function listRepos(Request $request) {
         $search = $request->only('search')['search'];
-        $endpoint = 'search/repositories';
-        $response = $this->github->request('GET', $endpoint, $search);
+        $response = $this->service->listRepos($search);
 
-        $body = json_decode($response->getBody(), true);
-        $status = $response->getStatusCode();
-
-        if ($status === 200)
-            return $this->responseSuccess($body['items']);
+        if ($response['status'] === 200)
+            return $this->responseSuccess($response['data']);
         
-        return $this->responseError($body, $status);
+        return $this->responseError($response['data'], $response['status']);
     }
 
     public function listReposUser(Request $request) {
         $ownerName = $request->only('ownerName')['ownerName'];
-        $endpoint = 'users/' . $ownerName . '/repos';
-        $response = $this->github->request('GET', $endpoint, NULL);
+        $response = $this->service->listReposUser($ownerName);
 
-        $body = json_decode($response->getBody(), true);
-        $status = $response->getStatusCode();
-
-        if ($status === 200)
-            return $this->responseSuccess($body);
+        if ($response['status'] === 200)
+            return $this->responseSuccess($response['data']);
         
-        return $this->responseError($body, $status);
+        return $this->responseError($response['data'], $response['status']);
     }
 }
